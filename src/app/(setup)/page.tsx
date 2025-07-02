b825bfb0495759f94e0eebcb24378034942b5d84
chengning <ningc673@gmail.com>
2025-07-03 00:19:05 +0800

    feat:initial modal

--- src/app/(setup)/page.tsx
+++ src/app/(setup)/page.tsx
@@ -1,9 +1,10 @@
 import { initialProfile } from "@/lib/initial-profile";
 import { db } from "@/lib/db";
 import { redirect } from "next/navigation";
+import { InitialModal } from "@/components/modals/initial-modal";
 
 const SetupPage = async () => {
-    const profile = initialProfile();
+    const profile = await initialProfile();
 
     const server = await db.server.findFirst({
         where: {
@@ -20,9 +21,7 @@ const SetupPage = async () => {
     }
 
     return ( 
-        <div>
-            create a server
-        </div>
+        <InitialModal />
      );
 }
  
